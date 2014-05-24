44e828f29db597d87d4efb46c43ab2359d81551c
Nick Johnstone <ncwjohnstone@gmail.com>
2014-05-24 14:13:27 +1200

    Broke tag slicing into get_tag method, added start method to Katana

--- katana.rb
+++ katana.rb
@@ -8,15 +8,19 @@ class Katana
     get_user_commands
   end
 
-  def get_user_commands
-    command = ARGV[0]
-    argument = ARGV[1]
+  def start(input)
+    route_user_command(input)
+  end
+
+  def route_user_command(input)
+    command, argument = input
+    
     case command
     when "try"
       if argument.nil?
         @attempt.try_random
-      elsif rgument.start_with?(':')
-        tag = argument.slice(1..-1)
+      elsif argument.start_with?(':')
+        tag = get_tag(argument)
         @attempt.try_tag(tag)
       else
         @attempt.try_specific(argument)
@@ -36,6 +40,13 @@ class Katana
       @attempt.help
     end
   end
+
+  def get_tag(argument)
+    argument.slice(1..-1)
+  end
+
+  private :route_user_command, :get_tag
 end
 
 katana = Katana.new
+katana.start(ARGV)
